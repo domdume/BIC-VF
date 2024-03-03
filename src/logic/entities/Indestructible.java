f0e3900f52ebae70b81677374d28b32b04ddc493
Eric <134229338+LKerdc@users.noreply.github.com>
2024-03-03 16:51:43 -0500

    Update Indestructible.java

--- src/logic/entities/Indestructible.java
+++ src/logic/entities/Indestructible.java
@@ -2,10 +2,22 @@ package logic.entities;
 
 public class Indestructible extends Entity {
 
+    /**
+     * Constructs an Indestructible object with the given parameters.
+     * 
+     * @param id the unique identifier of the entity
+     * @param x  the x-coordinate position of the entity
+     * @param y  the y-coordinate position of the entity
+     */
     public Indestructible(int id, int x, int y) {
         super("Indestructible", id, x, y);
     }
 
+    /**
+     * Retrieves the level ID associated with the Indestructible entity.
+     * 
+     * @return the level ID (always 8 for Indestructible entities)
+     */
     @Override
     public int getLevelId() {
         return 8;
