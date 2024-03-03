9eabfd76b9bd6b777827b83532787af08ed4f7c9
Eric <134229338+LKerdc@users.noreply.github.com>
2024-03-03 17:31:53 -0500

    Update Game.java

--- src/logic/Game.java
+++ src/logic/Game.java
@@ -23,6 +23,9 @@ public class Game {
     private boolean lastLevelCompleted;
     private boolean alive;
 
+    /**
+     * Constructs a new `Game` object, initializing its state.
+     */
     public Game() {
         this.totalScore = 0;
         this.running = false;
@@ -32,49 +35,102 @@ public class Game {
         this.alive = true;
     }
 
+    /**
+     * Gets the total score of the game.
+     * 
+     * @return The total score of the game.
+     */
     public int getTotalScore() {
         return this.totalScore;
     }
 
+    /**
+     * Gets the status of the last level completion.
+     * 
+     * @return True if the last level was completed, otherwise false.
+     */
     public boolean getLastLevelCompleted() {
         return this.lastLevelCompleted;
     }
 
+    /**
+     * Gets the status of the player's life.
+     * 
+     * @return True if the player is alive, otherwise false.
+     */
     public boolean getAlive() {
         return this.alive;
     }
 
+    /**
+     * Sets the status of the player's life.
+     * 
+     * @param val The value to set for the player's life status.
+     * @return The new value of the player's life status.
+     */
     private boolean setAlive(boolean val) {
         this.alive = val;
         return val;
     }
 
+    /**
+     * Sets the status of the last level completion.
+     * 
+     * @param val The value to set for the last level completion status.
+     * @return The new value of the last level completion status.
+     */
     private boolean setLastLevelCompleted(boolean val) {
         this.lastLevelCompleted = !val;
         return val;
     }
 
+     /**
+     * Gets the current score of the game.
+     * 
+     * @return The current score of the game.
+     */
     public int getScore() {
         return this.score;
     }
 
+    /**
+     * Gets the index of the current level.
+     * 
+     * @return The index of the current level.
+     */
     public int getCurrentLevelIndex() {
         return this.currentLevelIndex;
     }
 
+    /**
+     * Gets the list of entities in the game.
+     * 
+     * @return The list of entities in the game.
+     */
     public ArrayList<Entity> getEntities() {
         return this.entities;
     }
 
+    /**
+     * Checks if the game is currently running.
+     * 
+     * @return True if the game is running, otherwise false.
+     */
     public boolean getRunning() {
         return this.running;
     }
 
+    /**
+     * Resets the game to level 1 with a total score of 0.
+     */
     public void resetToLevel1() {
         this.currentLevelIndex = 1;
         this.totalScore = 0;
     }
 
+    /**
+     * Resets the current level and score.
+     */
     public void resetLevel() {
         currentLevelIndex -= currentLevelIndex > levelManager.getMaxLevel() ? 1 : 0;
         totalScore -= score;
@@ -91,17 +147,31 @@ public class Game {
         return moveResult ;
     }
 
+    /**
+     * Starts the game by setting it to running and generating entities for the current level.
+     */
     public void startGame() {
         this.running = true;
         this.generateEntities(this.levelManager.getLevel(this.currentLevelIndex), this.totalScore);
     }
 
+    /**
+     * Restarts the game by resetting entities and starting it again.
+     * 
+     * @return Always returns 0.
+     */
     public int restartGame() {
         this.entities = new ArrayList<Entity>();
         this.startGame();
         return 0;
     }
 
+    /**
+     * Handles collision with food entities, updating score and removing the food entity.
+     * 
+     * @param foodIndex The index of the food entity.
+     * @return Always returns 0.
+     */
     private int handleFoodCollition(int foodIndex) {
         this.score++;
         this.totalScore++;
@@ -109,6 +179,11 @@ public class Game {
         return 0;
     }
 
+    /**
+     * Sets the running state of the game.
+     * 
+     * @param val The value to set running state to.
+     */
     private void setRunning(boolean val) {
         this.running = val;
     }
@@ -133,14 +208,33 @@ public class Game {
         return handleResult ;
     }
 
+    /**
+     * Checks if there is a death collision between the player entity and other entities.
+     * 
+     * @param entity The player entity.
+     * @param i The index of the entity to check collision with.
+     * @return True if there is a death collision, otherwise false.
+     */
     private boolean isDeathCollition(Entity entity, int i) {
         return entity.handleCoalitions(this.entities.get(i)) instanceof Death;
     }
 
+    /**
+     * Checks if the player entity is collecting food.
+     * 
+     * @param entity The player entity.
+     * @param i The index of the entity to check for food collection.
+     * @return True if the player is collecting food, otherwise false.
+     */
     private boolean isCollectingFood(Entity entity, int i) {
         return entity.handleCoalitions(this.entities.get(i)) instanceof Points;
     }
 
+     /**
+     * Adjusts the level after completing a level.
+     * 
+     * @return The next level.
+     */
     private int adjustLevel() {
         this.currentLevelIndex++;
         this.currentLevelIndex = this.currentLevelIndex > this.levelManager.getMaxLevel()
@@ -150,6 +244,11 @@ public class Game {
         return levelAdjusted ;
     }
 
+     /**
+     * Checks the score of the game and adjusts the level accordingly.
+     * 
+     * @return the score.
+     */
     public int checkScore() {
         int scoreAdjusted  = this.score == this.currentLevel.getScore() ? this.adjustLevel() : 1;
         boolean isRunning = this.getRunning();
@@ -164,6 +263,11 @@ public class Game {
         return scoreAdjusted ;
     }
 
+    /**
+     * Checks for collisions between entities in the game.
+     * 
+     * @return Always returns 0.
+     */
     public int checkCollitions() {
         int collisionsChecked  = 0;
         for (Entity entity : this.entities) {
@@ -172,6 +276,12 @@ public class Game {
         return collisionsChecked ;
     }
 
+    /**
+     * Generates entities for the current level of the game.
+     * 
+     * @param level The current level of the game.
+     * @param score The score of the game.
+     */
     public void generateEntities(Level level, int score) {
         this.currentLevel = level;
         this.currentLevelIndex = this.currentLevel.getLevelIndex();
@@ -180,6 +290,11 @@ public class Game {
         this.entities = EntityGenerator.generate(this.currentLevel);
     }
 
+    /**
+     * Handles the actions to be taken when the game is running.
+     * 
+     * @return Always returns 0.
+     */
     private int handleRunningAction() {
         this.checkCollitions();
         this.moveEntities();
@@ -187,6 +302,11 @@ public class Game {
         return 0;
     }
 
+    /**
+     * Performs an action in the game, such as handling collisions and moving entities.
+     * 
+     * @return The action performed.
+     */
     public int actionPerformed() {
         return this.getRunning() ? this.handleRunningAction() : 0;
     }
