95e997e239a1468c6aa0ff19654d4deeec49e308
Eric <134229338+LKerdc@users.noreply.github.com>
2024-03-03 16:26:15 -0500

    Update Fruit.java

--- src/logic/entities/Fruit.java
+++ src/logic/entities/Fruit.java
@@ -1,10 +1,23 @@
 package logic.entities;
 
 public class Fruit extends Entity {
+    
+    /**
+     * Constructs a Fruit object with the given parameters.
+     * 
+     * @param id the unique identifier of the fruit
+     * @param x  the x-coordinate position of the fruit
+     * @param y  the y-coordinate position of the fruit
+     */
     public Fruit(int id, int x, int y) {
         super("Fruit", id, x, y);
     }
 
+    /**
+     * Gets the level identifier of the fruit.
+     * 
+     * @return the level identifier, 2 if id is 0, otherwise 3
+     */
     @Override
     public int getLevelId() {
         return this.id == 0 ? 2 : 3;
